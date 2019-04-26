5249f647d2b6c8b5d88447aaacd346b278bfe5bd
Ryan Gawick <rgawick@gmail.com>
2019-04-26 12:29:06 -0500

    gifs

--- client/src/components/Stripe.js
+++ client/src/components/Stripe.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux'
 
 class Stripe extends Component {
     onToken = (token) => {
-      fetch('http://localhost:8080/save-stripe-token', {
+      fetch('https://epicrideserver.herokuapp.com/save-stripe-token', {
         method: 'POST',
         headers: {
           'content-type': 'application/json'
