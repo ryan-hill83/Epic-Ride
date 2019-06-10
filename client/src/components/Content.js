a6eb6d0a6667ef90177882c71e43f5f95508a67e
Ryan Gawick <rgawick@gmail.com>
2019-06-10 16:09:43 -0500

    minor update

--- client/src/components/Content.js
+++ client/src/components/Content.js
@@ -9,34 +9,35 @@ export class Content extends Component {
     this.state = {
       selectedOption: 0
     }
-    
   }
 
   handleOptionChange = e => {
-    console.log(e.target.value)
     this.setState({
       selectedOption: e.target.value
     })
   }
 
+  handleSubmit = e => {
+    e.preventDefault()
+  }
+
   
   render() {
     let snowboardItems = this.props.snowboards.map((board,index) => {
-      return <div><form><li key={index} className="list_item"><div className="content_div1">
+      return <div><form onSubmit={this.handleSubmit}><li key={index} className="list_item"><div className="content_div1">
       <h3>{board.name}</h3>
       <h3>$ {board.price}</h3>
       <h4>{board.terrain}</h4> 
       <h4>Shape: {board.shape}</h4>
-      <p>Board Length:</p>
-      <select value={this.state.value} onChange={this.handleOptionChange}>
-      <option value="">Choose Length</option>
-      {board.length.map((item, index) => 
-            <option value={item} key={index}>{item}</option>
+      <p>Board Length:</p>    
+      <select value={this.state.selectedOption} onChange={this.handleOptionChange}>
+      {board.length.map((item, index) =>     
+            <option value={item} key={index}>{item}</option>     
       )}
-      </select>
+      </select> cm
       </div>
       <div className="content_div2">
-      <button className="content_button" type="button" onClick={() => this.props.addToCart({board}, this.state.selectedOption)}>Add to Cart</button>
+      <button className="content_button" type="submit" onClick={() => this.props.addToCart({board}, this.state.selectedOption)}>Add to Cart</button>
       <img className="image" src={board.imageurl} />
       </div>
       </li>
